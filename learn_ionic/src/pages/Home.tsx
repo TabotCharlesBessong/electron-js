791a2d8c3e06849cbbdefcc25d8e9d11f98bed3e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-12 19:58:48 +0100

    bootstrapping the application

---
+++ learn_ionic/src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>TABOT CHARLES BESSONG</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">HELLO MY BOY</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
