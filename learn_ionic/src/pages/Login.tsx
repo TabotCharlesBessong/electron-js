6dead103fa9b5d34d46cd449553f29b800e60f19
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-14 11:03:12 +0100

    ionic grid

--- learn_ionic/src/pages/Login.tsx
+++ learn_ionic/src/pages/Login.tsx
@@ -3,12 +3,15 @@ import {
   IonButton,
   IonCard,
   IonCardContent,
+  IonCol,
   IonContent,
   IonFooter,
+  IonGrid,
   IonHeader,
   IonIcon,
   IonInput,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
   useIonLoading,
@@ -34,7 +37,7 @@ const Login: React.FC = () => {
     setTimeout(async () => {
       setTimeout(async () => {
         await dismiss();
-      }, 2000);
+      }, 500);
     }, 2000);
     router.push("/app", "forward");
   };
@@ -63,13 +66,6 @@ const Login: React.FC = () => {
         <Intro onFinish={finishIntro} />
       ) : (
         <IonPage>
-          <img
-            src={logo}
-            alt="freecodecamp"
-            width={200}
-            height={150}
-            style={{ margin: "1rem auto" }}
-          />
           <IonHeader>
             <IonToolbar color="success">
               <IonButton>
@@ -78,51 +74,68 @@ const Login: React.FC = () => {
               <IonTitle>Login Screen</IonTitle>
             </IonToolbar>
           </IonHeader>
-          <IonContent scrollY={false}>
-            <IonCard>
-              <IonCardContent>
-                <form action="" onSubmit={onLogin}>
-                  <IonInput
-                    label="Email"
-                    type="email"
-                    placeholder="charles@gmail.com"
-                    labelPlacement="floating"
-                    fill="outline"
-                  ></IonInput>
-                  <IonInput
-                    className="ion-margin-top"
-                    label="Password"
-                    type="password"
-                    placeholder="************"
-                    labelPlacement="floating"
-                    fill="outline"
-                  ></IonInput>
-                  <IonButton type="submit" expand="full">
-                    Login <IonIcon icon={logInOutline} slot="end" />{" "}
-                  </IonButton>
-                  <IonButton
-                    color="secondary"
-                    routerLink="/register"
-                    expand="full"
-                  >
-                    Create account{" "}
-                    <IonIcon icon={personCircleOutline} slot="end" />{" "}
-                  </IonButton>
-                  <IonButton
-                    onClick={seeIntroAgain}
-                    fill="clear"
-                    size="small"
-                    color={"medium"}
-                    type="button"
-                    expand="block"
-                    className="ion-margin-top"
-                  >
-                    Watch intro again
-                    <IonIcon icon={personCircleOutline} slot="end" />
-                  </IonButton>
-                </form>
-              </IonCardContent>
-            </IonCard>
+          <IonContent scrollY={false} className="ion-padding">
+            <IonGrid fixed>
+              <IonRow className="ion-justify-content-center">
+                <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+                  <img
+                    src={logo}
+                    alt="freecodecamp"
+                    width={"70%"}
+                    height={150}
+                    style={{ margin: "1rem auto" }}
+                  />
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-justify-content-center">
+                <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+                  <IonCard>
+                    <IonCardContent>
+                      <form action="" onSubmit={onLogin}>
+                        <IonInput
+                          label="Email"
+                          type="email"
+                          placeholder="charles@gmail.com"
+                          labelPlacement="floating"
+                          fill="outline"
+                        ></IonInput>
+                        <IonInput
+                          className="ion-margin-top"
+                          label="Password"
+                          type="password"
+                          placeholder="************"
+                          labelPlacement="floating"
+                          fill="outline"
+                        ></IonInput>
+                        <IonButton type="submit" expand="full">
+                          Login <IonIcon icon={logInOutline} slot="end" />{" "}
+                        </IonButton>
+                        <IonButton
+                          color="secondary"
+                          routerLink="/register"
+                          expand="full"
+                        >
+                          Create account{" "}
+                          <IonIcon icon={personCircleOutline} slot="end" />{" "}
+                        </IonButton>
+                        <IonButton
+                          onClick={seeIntroAgain}
+                          fill="clear"
+                          size="small"
+                          color={"medium"}
+                          type="button"
+                          expand="block"
+                          className="ion-margin-top"
+                        >
+                          Watch intro again
+                          <IonIcon icon={personCircleOutline} slot="end" />
+                        </IonButton>
+                      </form>
+                    </IonCardContent>
+                  </IonCard>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
           </IonContent>
           <IonFooter>
             <IonToolbar>footer</IonToolbar>
--- learn_ionic/src/pages/Register.tsx
+++ learn_ionic/src/pages/Register.tsx
@@ -3,12 +3,15 @@ import {
   IonButton,
   IonCard,
   IonCardContent,
+  IonCol,
   IonContent,
   IonFooter,
+  IonGrid,
   IonHeader,
   IonIcon,
   IonInput,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
   useIonRouter,
@@ -22,18 +25,11 @@ const Register: React.FC = () => {
   const onRegister = (event: any) => {
     event.preventDefault();
     console.log("am in your house");
-    router.goBack()
+    router.goBack();
   };
 
   return (
     <IonPage>
-      <img
-        src={logo}
-        alt="freecodecamp"
-        width={200}
-        height={150}
-        style={{ margin: "1rem auto" }}
-      />
       <IonHeader>
         <IonToolbar color="success">
           <IonButton>
@@ -43,42 +39,60 @@ const Register: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent scrollY={false}>
-        <IonCard>
-          <IonCardContent>
-            <form action="" onSubmit={onRegister} >
-              <IonInput
-                label="Email"
-                type="email"
-                placeholder="charles@gmail.com"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-              <IonInput
-                className="ion-margin-top"
-                label="Password"
-                type="password"
-                placeholder="************"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-              <IonInput
-                className="ion-margin-top ion-margin-bottom"
-                label="Confirm Password"
-                type="password"
-                placeholder="************"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-              <IonButton expand="full" type="submit">
-                Create account <IonIcon icon={personCircleOutline} slot="end" />{" "}
-              </IonButton>
-              <IonButton color="secondary" routerLink="/" expand="full">
-                Log in to your account
-                <IonIcon icon={logInOutline} slot="end" />{" "}
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
+        <IonGrid fixed>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <img
+                src={logo}
+                alt="freecodecamp"
+                width={"70%"}
+                height={100}
+                style={{ margin: "1rem auto" }}
+              />
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <IonCard>
+                <IonCardContent>
+                  <form action="" onSubmit={onRegister}>
+                    <IonInput
+                      label="Email"
+                      type="email"
+                      placeholder="charles@gmail.com"
+                      labelPlacement="floating"
+                      fill="outline"
+                    ></IonInput>
+                    <IonInput
+                      className="ion-margin-top"
+                      label="Password"
+                      type="password"
+                      placeholder="************"
+                      labelPlacement="floating"
+                      fill="outline"
+                    ></IonInput>
+                    <IonInput
+                      className="ion-margin-top ion-margin-bottom"
+                      label="Confirm Password"
+                      type="password"
+                      placeholder="************"
+                      labelPlacement="floating"
+                      fill="outline"
+                    ></IonInput>
+                    <IonButton expand="full" type="submit">
+                      Create account{" "}
+                      <IonIcon icon={personCircleOutline} slot="end" />{" "}
+                    </IonButton>
+                    <IonButton color="secondary" routerLink="/" expand="full">
+                      Log in to your account
+                      <IonIcon icon={logInOutline} slot="end" />{" "}
+                    </IonButton>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
       <IonFooter>
         <IonToolbar>footer</IonToolbar>
