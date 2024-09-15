69343ab19bcf7ba90dc972dda2381bf69ed2eb10
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-15 10:49:01 +0100

    ionic components

--- learn_ionic/src/pages/List.tsx
+++ learn_ionic/src/pages/List.tsx
@@ -5,11 +5,28 @@ import {
   IonMenuButton,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  useIonViewWillEnter
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 
 const List: React.FC = () => {
+  const [users, setUsers] = useState<any []>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const getUsers = async () => {
+    const data = await fetch("https://randomuser.me/api?results=15");
+    const user = await data.json();
+    return user;
+  };
+
+  useIonViewWillEnter(async () => {
+    setLoading(true)
+    const users = await getUsers()
+    setUsers(users)
+    setLoading(false)
+  })
+  
   return (
     <IonPage>
       <IonHeader>
